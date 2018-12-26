91f6f5fac8f60230d7e7361bc7aa2cf32469a617
Andrey Makuhin <a_makuhin@mail.ru>
2018-12-26 22:01:14 +0500

    добавил функцию read()

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,11 +9,26 @@ void print(const char* c)
 	}
 }
 
+void read(char* buf, int buf_size)
+{
+	char* end = buf + buf_size - 1;
+	for (char c = _getch(); c != 13 && buf < end; c = _getch(), buf++)
+	{
+		_putch(c);
+		*buf = c;
+	}
+	*buf = 0;
+}
+
 int main()
 {
-	char msg[] = "Hello? world!";
+	char msg[] = "Hello, world!";
+	char buffer[69];
 
-	print(msg);
+	read(buffer, 69);
+
+	print("\n");
+	print(buffer);
 
 	while (!_kbhit());
 
